b82ad09c7e661bae6cbb65e378283f57fcd350dd
Staffa01 <jzuken01@gmail.com>
2023-06-24 14:11:10 +0600

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/main/java/ru/job4j/For1.java
    
     Untracked files:
    	src/main/java/ru/job4j/Calculator

--- src/main/java/ru/job4j/For1.java
+++ src/main/java/ru/job4j/For1.java
@@ -3,8 +3,8 @@ package ru.job4j;
 public class For1 {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <=9 ; j++) {
-                System.out.println(i+"*"+j+"="+(i*j));
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + "*" + j + "=" + (i*j));
             }
 
         }
